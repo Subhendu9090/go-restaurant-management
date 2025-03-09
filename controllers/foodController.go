d7f69805e8eda2459501d853a8fb2fa6cee0f640
“subhendu <subhendujena38@gmail.com>
2025-03-09 10:41:59 +0530

    added menu controller

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,16 +1,83 @@
 package controllers
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/models"
+	"net/http"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.GetCollectionName("food")
+var validate = validator.New()
 
 func GetFoods(w http.ResponseWriter, r *http.Request) {
 
 }
 func GetFood(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	params := mux.Vars(r)
+	foodId := params["food_id"]
+	var food models.Food
 
+	err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+	defer cancel()
+
+	if err != nil {
+		http.Error(w, "Error in finding food ", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content/type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(food)
 }
+
 func CreateFood(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	var menu models.Menu
+	var food models.Food
+	err := json.NewDecoder(r.Body).Decode(&food)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	err = validate.Struct(food)
+	if err != nil {
+		http.Error(w, " validation Error", http.StatusForbidden)
+		return
+	}
+	err = menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+	defer cancel()
+	if err != nil {
+		http.Error(w, "Menu not Found", http.StatusNotFound)
+		return
+	}
+	food.Created_at = time.Now()
+	food.Updated_at = time.Now()
 
+	food.Id = primitive.NewObjectID()
+	food.Food_id = food.Id.Hex()
+	// var num = toFixed(*food.Price, 2)
+	// food.Price = &num
+
+	result, err := foodCollection.InsertOne(ctx, food)
+	if err != nil {
+		http.Error(w, "error in creating food", 400)
+	}
+	defer cancel()
+	w.Header().Set("Content/type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(result)
 }
+
 func UpdateFoods(w http.ResponseWriter, r *http.Request) {
 
 }
--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,16 +1,133 @@
 package controllers
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/models"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var menuCollection *mongo.Collection = database.GetCollectionName("Menu")
 
 func GetMenus(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	result, err := menuCollection.Find(ctx, bson.M{})
 
-}
-func CreateMenu(w http.ResponseWriter, r *http.Request) {
+	if err != nil {
+		http.Error(w, err.Error(), 404)
+		return
+	}
+	var allMenu []bson.M
+	err = result.All(ctx, &allMenu)
 
+	if err != nil {
+		http.Error(w, err.Error(), 401)
+		return
+	}
+	w.Header().Set("Content/type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(allMenu)
 }
-func UpdateMenu(w http.ResponseWriter, r *http.Request) {
 
-}
 func GetMenu(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	params := mux.Vars(r)
+	menuId := params["menu_id"]
+	var menu models.Menu
+	err := menuCollection.FindOne(ctx, bson.M{"menu_id": menuId}).Decode(&menu)
 
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(menu)
+}
+
+func CreateMenu(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	var menu models.Menu
+	err := json.NewDecoder(r.Body).Decode(&menu)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	validateErr := validate.Struct(menu)
+	if validateErr != nil {
+		http.Error(w, "Validation Error", http.StatusBadRequest)
+		return
+	}
+	menu.Created_at = time.Now()
+	menu.Updated_at = time.Now()
+	menu.Id = primitive.NewObjectID()
+	menu.Menu_id = menu.Id.Hex()
+	result, inserterr := menuCollection.InsertOne(ctx, &menu)
+	if inserterr != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, "Inserting Error", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(result)
+}
+
+func UpdateMenu(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	var menu models.Menu
+	if err := json.NewDecoder(r.Body).Decode(&menu); err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, `{"error": "Invalid request body"}`, http.StatusBadRequest)
+		return
+	}
+
+	params := mux.Vars(r)
+	menuId := params["menu_id"]
+
+	update := bson.M{
+		"$set": bson.M{
+			"name":       menu.Name,
+			"category":   menu.Category,
+			"start_date": menu.Start_date,
+			"end_date":   menu.End_date,
+			"updated_at": time.Now(),
+		},
+	}
+
+	var updatedMenu models.Menu
+	err := menuCollection.FindOneAndUpdate(
+		ctx,
+		bson.M{"menu_id": menuId},
+		update,
+		options.FindOneAndUpdate().SetReturnDocument(options.After),
+	).Decode(&updatedMenu)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		if err == mongo.ErrNoDocuments {
+			http.Error(w, `{"error": "Menu not found"}`, http.StatusNotFound)
+		} else {
+			http.Error(w, `{"error": "Failed to update menu"}`, http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updatedMenu)
 }
