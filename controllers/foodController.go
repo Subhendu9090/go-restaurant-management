d399d69d43ed6e217d57b24c39ff36198a0e075c
“subhendu <subhendujena38@gmail.com>
2025-03-15 21:27:07 +0530

    added get order and get order by id api

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -6,6 +6,7 @@ import (
 	"golang-restaurant-management/database"
 	"golang-restaurant-management/models"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/go-playground/validator/v10"
@@ -13,14 +14,59 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var foodCollection *mongo.Collection = database.GetCollectionName("food")
 var validate = validator.New()
 
 func GetFoods(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	params := mux.Vars(r)
+	recordPerPage, err := strconv.Atoi(params["recordPerPage"])
+	if err != nil || recordPerPage < 1 {
+		recordPerPage = 10
+	}
+
+	page, err := strconv.Atoi(params["page"])
+	if err != nil || page < 1 {
+		page = 1
+	}
+
+	startIndex := (page - 1) * recordPerPage
+
+	totalCount, err := foodCollection.CountDocuments(ctx, bson.M{})
+	if err != nil {
+		http.Error(w, "Error counting foods", http.StatusInternalServerError)
+		return
+	}
+
+	opts := options.Find().SetSkip(int64(startIndex)).SetLimit(int64(recordPerPage))
+	crusor, err := foodCollection.Find(ctx, bson.M{}, opts)
+	if err != nil {
+		http.Error(w, "Error fetching foods", http.StatusInternalServerError)
+		return
+	}
+	var foods models.Food
+	err = crusor.All(ctx, &foods)
+	if err != nil {
+		http.Error(w, "Error fetching foods", http.StatusInternalServerError)
+		return
+	}
+	// Create response structure
+	response := map[string]interface{}{
+		"totalItems": totalCount,
+		"page":       page,
+		"perPage":    recordPerPage,
+		"data":       foods,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 
 }
+
 func GetFood(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 	defer cancel()
@@ -79,5 +125,57 @@ func CreateFood(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateFoods(w http.ResponseWriter, r *http.Request) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	var food models.Food
 
+	params := mux.Vars(r)
+	foodId := params["food_id"]
+	err := json.NewDecoder(r.Body).Decode(&food)
+
+	if err != nil {
+		http.Error(w, "Error counting foods", http.StatusInternalServerError)
+		return
+	}
+	// Dynamically create update fields
+	updateFields := bson.M{}
+	if food.Name != nil && *food.Name != "" {
+		updateFields["name"] = food.Name
+	}
+	if *food.Price > 0 {
+		updateFields["price"] = food.Price
+	}
+	if food.Food_image != nil && *food.Food_image != "" {
+		updateFields["food_image"] = food.Food_image
+	}
+
+	// If no valid fields were provided, return an error
+	if len(updateFields) == 0 {
+		http.Error(w, `{"error": "No valid fields provided for update"}`, http.StatusBadRequest)
+		return
+	}
+
+	// Always update the timestamp
+	updateFields["updated_at"] = primitive.NewDateTimeFromTime(time.Now())
+
+	update := bson.M{"$set": updateFields}
+
+	// update := bson.M{
+	// 	"$set": bson.M{
+	// 		"name":       food.Name,
+	// 		"price":      food.Price,
+	// 		"food_image": food.Food_image,
+	// 		"updated_at": time.Now(),
+	// 	},
+	// }
+	var updatedFood models.Food
+	err = foodCollection.FindOneAndUpdate(ctx, bson.M{"food_id": foodId}, update, options.FindOneAndUpdate().SetReturnDocument(options.After)).Decode(&updatedFood)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, `{"error": "Failed to update menu"}`, http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updatedFood)
 }
--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,16 +1,62 @@
 package controllers
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"golang-restaurant-management/database"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var orderCollection *mongo.Collection = database.GetCollectionName("order")
 
 func GetOrders(w http.ResponseWriter, r *http.Request) {
-
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*100)
+	var orders []bson.M
+	defer cancel()
+	result, err := orderCollection.Find(ctx, bson.M{})
+	if err != nil {
+		http.Error(w, "Error in finding Orders", http.StatusNotFound)
+		return
+	}
+	err = result.All(ctx, &orders)
+	if err != nil {
+		http.Error(w, "Error in Decoding Orders", http.StatusForbidden)
+		return
+	}
+	res, err := json.Marshal(orders)
+	if err != nil {
+		http.Error(w, "Marshalling error", http.StatusForbidden)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
+func GetOrder(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	params := mux.Vars(r)
+	orderId := params["order_id"]
+	if orderId == "" {
+		http.Error(w, "Order Id Not found", http.StatusNotFound)
+		return
+	}
+	var order bson.M
+	err := orderCollection.FindOne(ctx, bson.M{"order_id": orderId}).Decode(&order)
+	if err != nil {
+		http.Error(w, "Error in Finding order", http.StatusNotFound)
+	}
+	json.NewEncoder(w).Encode(order)
+}
+
 func CreateOrder(w http.ResponseWriter, r *http.Request) {
 
 }
-func UpdateOrder(w http.ResponseWriter, r *http.Request) {
 
-}
-func GetOrder(w http.ResponseWriter, r *http.Request) {
+func UpdateOrder(w http.ResponseWriter, r *http.Request) {
 
 }
